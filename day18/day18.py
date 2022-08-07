bd6d826591bda1ffef53070196a930179abfb371
GeMixl <g.michl@gmx.net>
2022-08-07 12:39:44 +0200

    Solved day 18

--- day18/day18.py
+++ day18/day18.py
@@ -4,93 +4,95 @@
 # Puzzle input at https://adventofcode.com/2021/day/18/input  #
 ###############################################################
 
-from collections import deque
-
-with open("test_day18.txt", "r") as fs:
-    data = [i for i in fs.readline()]
-
-data = deque(data)
+# with a lot of help from bwerner!!!
+# https://github.com/benediktwerner
 
 
-class Node:
-    def __init__(self, p=None):
-        self.left_node = None
-        self.right_node = None
-        self.parent_node = p
+from math import ceil
+from functools import reduce
+from itertools import permutations
 
 
-class Leaf(Node):
-    def __init__(self, p, v):
-        Node.__init__(self, p)
-        self.val = v
-        self.right_leaf = None
-        self.left_leaf = None
-
-
-def print_nodes(dat):
-    res = []
-    if type(dat.left_node) == Node:
-        res += ["[",  print_nodes(dat.left_node)]
-    else:
-        res += ["[",  str(dat.left_node.val)]
-    if type(dat.right_node) == Node:
-        res += [print_nodes(dat.right_node), "]"]
-    else:
-        res = res + [",", str(dat.right_node.val), "]"]
-    return "".join(res)
-
-
-def read_binTree(dat, p=None):
-    d = dat.popleft()
+def read_binTree(it, n=0):
+    d = next(it)
     if d == "[":
-        n = Node(p)
-        n.left_node = read_binTree(dat, n)
-        d = dat.popleft()
+        n += 1
+        left_node = read_binTree(it, n)
+        d = next(it)
     if d in "0123456789":
-        return Leaf(p, int(d))
+        return int(d)
     if d == ",":
-        n.right_node = read_binTree(dat, n)
-        d = dat.popleft()
+        right_node = read_binTree(it, n)
+        d = next(it)
     if d == "]":
-        return n
+        n -= 1
+    return [left_node, right_node]
 
 
-def neighborize_binTree(n: Node, payload: Leaf = None):
-    if type(n) is Leaf:
-        n.left_leaf = payload
-        if payload is not None:
-            payload.right_leaf = n
-        return n
-    payload = neighborize_binTree(n.left_node, payload)
-    payload = neighborize_binTree(n.right_node, payload)
-    return payload
+def add_to_left(x, n):
+    if n is None:
+        return x
+    if isinstance(x, int):
+        return x + n
+    return [add_to_left(x[0], n), x[1]]
 
 
-def add_node(n1: Node, n2: Node) -> Node:
-    n = Node(None)
-    n.left_node, n.right_node = n1, n2
-    n.left_node.parent_node = n
-    n.right_node.parent_node = n
-    return n
+def add_to_right(x, n):
+    if n is None:
+        return x
+    if isinstance(x, int):
+        return x + n
+    return [x[0], add_to_right(x[1], n)]
 
 
-def explode_node(n: Node, level: int = 0) -> int:
-    level += 1
-    if type(n.left_node) is Leaf and type(n.right_node) is Leaf and level > 4:
-        print(f"found number pair {n.left_node.val}, {n.right_node.val} at level = {level+1}")
-        return True
-    if explode_node(n.left_node, level):
-        pass
-        #print(f"redistribute {n.left_node.val} to {n.left_node.left_leaf}")
-    if explode_node(n.right_node, level):
-        pass
-        #print(f"redistribute {n.right_node.val} to {n.right_node.right_leaf}")
+def explode_snailfish_number(x, n=0) -> (bool, int, [], int):
+    if isinstance(x, int):
+        return False, None, x, None
+    l, r = x
+    if n >= 4:
+        return True, l, 0, r
+    exp, a, l, b = explode_snailfish_number(l, n+1)
+    if exp:
+        return True, a, [l, add_to_left(r, b)], None
+    exp, a, r, b = explode_snailfish_number(r, n+1)
+    if exp:
+        return True, None, [add_to_right(l, a), r], b
+    return False, None, x, None
 
 
-myNode1 = read_binTree(data)
-myNode2 = read_binTree(deque("[1,2]"))
-myNode3 = add_node(myNode1, myNode2)
-print(print_nodes(myNode3))
-neighborize_binTree(myNode3)
-explode_node(myNode1)
-print(print_nodes(myNode3))
+def split_snailfish_number(x):
+    if isinstance(x, int):
+        if x >= 10:
+            return True, [x // 2, ceil(x / 2)]
+        return False, x
+    a, b = x
+    change, a = split_snailfish_number(a)
+    if change:
+        return True, [a, b]
+    change, b = split_snailfish_number(b)
+    return change, [a, b]
+
+def add_snailfish_number(a, b):
+    res =  [a, b]
+    while True:
+        change, _, res, _ = explode_snailfish_number(res)
+        if change:
+            continue
+        change, res = split_snailfish_number(res)
+        if not change:
+            break
+    return res
+
+
+def magnitude(x):
+    if isinstance(x, int):
+        return x
+    return 3 * magnitude(x[0]) + 2 * magnitude(x[1])
+
+
+with open("input_day18.txt", "r") as fs:
+    data = map(str.strip, fs)
+    result = [read_binTree(iter(line)) for line in data]
+    print(result[0])
+    print("Puzzle 1: {}".format(magnitude(reduce(add_snailfish_number, result))))
+    print("Puzzle 2: {}".format(max(magnitude(add_snailfish_number(a, b)) for a, b in permutations(result, 2))))
